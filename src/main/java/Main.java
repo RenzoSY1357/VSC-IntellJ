3d5598f6392a5e7af37f58aa7d0b1fe2c00cd25c
Renzo <renzosy1357@gmail.com>
2025-02-13 09:59:20 +0100

    Primer commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+package main.java;
+
+public class Main {
+    private int num1;
+    private int num2;
+    public Main(int a, int b) {
+        num1 = a;
+        num2 = b;
+    }
+public int suma() {
+        int resul = num1 + num2;
+        return resul;
+    }
+    public int resta() {
+        int resul = num1 - num2;
+        return resul;
+    }
+    public int multiplica() {
+        int resul = num1 * num2;
+        return resul;
+    }
+    public int divide() {
+        int resul = num1 / num2;
+        return resul;
+    }
+    public int divide0() {
+        if(num2 == 0)
+            throw new java.lang.ArithmeticException("División por 0");
+        else{
+            int resul= num1 / num2;
+            return resul;
+        }
+    }
+}
